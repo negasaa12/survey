755c2adc4a58631f4a1aab2af915d2fbe4463a3a
kelly <sntsklvn@gmail.com>
2023-03-20 16:57:32 -0400

    using session

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask , request, render_template , session, redirect, url_for
+from flask import Flask , request, render_template , session, redirect, url_for, flash
 from survey import Question, Survey, satisfaction_survey as survey, personality_quiz
 from flask_debugtoolbar import DebugToolbarExtension
+
+
+keys = 'responses'
 
 
 app = Flask(__name__)
@@ -8,7 +11,17 @@ app.config['SECRET KEY'] = 'hello'
 app.debug = True
 debug = DebugToolbarExtension
 
-responses = []
+
+
+
+
+@app.route('/begin', methods=["POST"])
+def handle_responses():
+   
+   session[keys] = []
+   
+   return redirect('/questions/0')
+
 
 @app.route('/')
 def home():
@@ -18,18 +31,33 @@ def home():
     return render_template('home.html', instructions=instructions, title=title)
 
 
-@app.route('/questions/<int:question_id>', methods=['POST','GET']) 
+@app.route('/questions/<int:question_id>') 
 def survey_question(question_id):
 
-    
+    responses = session.get(keys)
+
     questions_list = survey.questions
     question = questions_list[question_id]
     
-    if question_id < 1 or len(questions_list) -1:
-        # return redirect(f'/questions/{question_id + 1}')
-        # print(f"THIS IS QUESTION ===> {question.question}")
+    # if (question_id < 1 or len(questions_list) -1):
+       
+
+    if (responses is None):
+         
+         return redirect('/')
+    
+    if (len(responses) == len(survey.questions)):
+         
+         return redirect ("/complete")
         
-         return render_template('questions.html', questions_list=questions_list, question=question )
+    if (len(responses) != question_id):
+        
+        flash(f"INVALID QUESTION ID : {question_id}.")
+        
+        return redirect(f"/question/{len(responses)}")
+            
+    
+    return render_template('questions.html', questions_list=questions_list, question=question )
 
 
 @app.route('/answers', methods=["POST"] )
@@ -39,16 +67,22 @@ def your_answer():
     
     # question = questions_list[id]
     answer = request.form['the-answers']
+    responses = session[keys]
     responses.append(answer)
+    session[keys] = responses 
+
     print(responses)
 
-    if len(responses) == len(survey.questions):
+    if (len(responses) == len(survey.questions)):
         return redirect("/complete")
+    
     else:
 
         return redirect(f"/questions/{len(responses)}")
 
+
+
 @app.route('/complete')
 def complete():
-    
+    """SURVEY COMPLETE. SHOWS COMPLETE PAGE."""
     return render_template('complete.html')
